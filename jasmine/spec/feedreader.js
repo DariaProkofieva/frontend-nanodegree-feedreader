507bc5f4e5abc7c4ecaf94037d3d1c3344320e4e
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-06-03 14:34:18 +0200

    Added two spects to test the Menu
    Tests checks:
    1. the menu element is hidden by default.
    2. the menu changes visibility when the menu icon is clicked.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,19 +52,30 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+    describe('The menu', function() {
+         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+          it('is hidden by default', function() {
+           let hiddenMenu=document.body.classList.contains('menu-hidden');
+           expect(hiddenMenu).toBe(true);
+         });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+          it('displays when clicked and hide when clicked again', function() {
+            //First click
+            $('.menu-icon-link').trigger('click');
+              expect(document.body.classList.contains('menu-hidden')).toBe(false);
+            //Second click
+            $('.menu-icon-link').trigger('click');
+              expect(document.body.classList.contains('menu-hidden')).toBe(true);
+          });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
